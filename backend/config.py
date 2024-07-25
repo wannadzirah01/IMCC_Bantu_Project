16795be20d3d624629fc20cbbd5200c896488924
wannadzirah01 <wannadzirah.wa@gmail.com>
2024-07-25 13:25:21 +0800

    Update database and redis configuration

--- backend/config.py
+++ backend/config.py
@@ -8,14 +8,16 @@ load_dotenv()
 class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
 
+    # Database configuration
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/imcc_bantu_project'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'mysql://root:@localhost/imcc_bantu_project')
 
+    # Redis configuration
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    SESSION_REDIS = redis.from_url(os.environ.get('REDIS_URL', "redis://127.0.0.1:6379"))
 
     # Mail server configuration
     MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
